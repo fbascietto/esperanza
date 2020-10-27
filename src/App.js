e249e47bc56f0848931d7db2129aa2d712529233
fbascietto <fran.ba22@gmail.com>
2020-10-27 12:09:30 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import Countdown from 'react-countdown';
 import './App.css';
+
+const Completionist = () => <span>¿Qué haces mirando esto? Andá a la quinta!</span>;
+ 
+// Renderer callback with condition
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a completed state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    const text = `Faltan ${days} días, ${hours} horas, ${minutes} minutos y ${seconds} segundos.`;
+    return <span>{text}</span>;
+  }
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        <h1>
+          Cuadrazos, Cañuelas en:
+        </h1>
+        <Countdown date={'2020-12-08T16:00:00'} renderer={renderer}/>
+        <div className='Item'>
+          <a
+            className="link"
+            href="https://docs.google.com/spreadsheets/d/1_4J21WzATOdeTBdpbAU_euGrJKQCbGvxqC9EdgN2TZQ/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className='Tano-logo'></div>
+          </a>
+          <a
+          className="link"
+          href="https://discord.gg/Pbbn2CE"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <div className='Discord-logo'></div>
         </a>
+        </div>
       </header>
     </div>
   );
